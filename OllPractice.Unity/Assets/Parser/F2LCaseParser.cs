ac884dd7fc90d3f2f562cc7a195c34be72aa4192
Bacskay Viktor <bacskay.viktor@gmail.com>
2021-03-30 18:31:10 +0200

    Add f2l parser

---
+++ OllPractice.Unity/Assets/Parser/F2LCaseParser.cs
@@ -0,0 +1,40 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Parser
+{
+    public sealed class F2LCaseParser
+    {
+        public static List<F2LCase> LoadJson()
+        {
+            var jsonFile = Resources.Load<TextAsset>("Cases/F2LCases");
+
+            return JsonUtility.FromJson<F2LCaseList>(jsonFile.text).F2LCases;
+
+        }
+
+        [Serializable]
+        private sealed class F2LCaseList
+        {
+            public List<F2LCase> F2LCases;
+        }
+        [Serializable]
+        public sealed class F2LCase
+        {
+            public string Name;
+            public List<string> Faces;
+            public string Solution;
+            public List<Face> GetStateFromFaces()
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<Rotation> GetSolution()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ OllPractice.Unity/Assets/Scripts/Loaders/F2LLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Parser;
+using UnityEngine;
+
+namespace Loaders
+{
+    public class F2LLoader : MonoBehaviour
+    {
+        private F2LCaseParser _f2lParser;
+        private List<F2LCaseParser.F2LCase> _cases;
+
+        public void Start()
+        {
+            _f2lParser = new F2LCaseParser();
+        }
+
+        public void LoadF2LCase()
+        {
+            _cases = F2LCaseParser.LoadJson();
+        }
+
+    }
+}
